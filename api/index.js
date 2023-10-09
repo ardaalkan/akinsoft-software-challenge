a35e0745f50debe6290140ea823e060441a92ad3
ardaalkan <ardaaa.alk@gmail.com>
2023-10-10 01:58:57 +0300

    test mongodb with user models

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -21,4 +22,6 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+app.use(express.json()),
 app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
