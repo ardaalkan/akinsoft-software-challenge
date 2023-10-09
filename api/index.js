67d4bd7dccae41a0a5fea45a7372cb6fa696a475
ardaalkan <ardaaa.alk@gmail.com>
2023-10-09 19:05:27 +0300

    test section api routing before auth

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("Connected MongoDB!");
-}).catch((err) => {
-  console.log(err);
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 //Sunucuyu ayağa kaldırmak için gerekli satırlar.
 //Nodemon paketi ile aktif server dinleme sağlanır.
 const app = express();
@@ -16,3 +20,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
