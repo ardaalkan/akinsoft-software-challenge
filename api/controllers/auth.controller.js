9e646588ad5a8ceb8b36f353aa5df4f90dbfebb1
ardaalkan <ardaaa.alk@gmail.com>
2023-10-10 03:05:17 +0300

    added error middleware code

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,21 @@
 import User from "../models/user.model.js";
+import md5 from "md5";
 
-export const signup = async (req, res) => {
+/*
+md5 şifreleme paketi ile birlikte kullanıcının gireceği şifre artık belirli bir
+algoritmaya bağlı kalarak şifrelenecek ve şifre veritabanına yani mongodb'ye şifrelenmiş
+bir şekilde gönderilecek ve veritabanında şifrelenmiş şekilde görüntülenecek.
+*/
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("User created succesfully!");
+  const hashedPassword = md5(password);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created succesfully!");
+  } catch (error) {
+    next(error);
+    // res.status(500).json(error.message);
+  }
 };
