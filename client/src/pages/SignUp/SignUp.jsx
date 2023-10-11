a9c02d1793f2fa073679d3cd44b0d9f17038aa05
ardaalkan <ardaaa.alk@gmail.com>
2023-10-11 18:07:17 +0300

    fixed sign-up routing

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,36 +1,84 @@
+import { useState } from "react";
 import styles from "./SignUp.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  console.log(formData);
+
   return (
     <div className={styles.pageContainer}>
-    <div className={styles.signUp}>
-      <h1>Sign Up</h1>
-      <form className={styles.form}>
-        <input
-          type="text"
-          placeholder="Username"
-          className={styles.usernameInput}
-          id="username"
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          className={styles.usernameInput}
-          id="email"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className={styles.usernameInput}
-          id="password"
-        />
-        <button className={styles.buttonSignUp}>Sign Up</button>
-      </form>
-      <Link to={'/sign-in'}>
-        <h2>Sign In</h2>
-      </Link>
-    </div>
+      <div className={styles.signUp}>
+        <h1>Sign Up</h1>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            className={styles.usernameInput}
+            id="username"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className={styles.usernameInput}
+            id="email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.usernameInput}
+            id="password"
+            onChange={handleChange}
+          />
+          <button disabled={loading} className={styles.buttonSignUp}>
+            {loading ? "Loading" : "Sign Up"}
+          </button>
+        </form>
+        <Link to={"/sign-in"}>
+          <h2>Sign In</h2>
+        </Link>
+      </div>
+      {error && <p>{error}</p>}
     </div>
   );
 }
