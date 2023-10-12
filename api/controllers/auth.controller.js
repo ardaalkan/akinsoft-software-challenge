7d9a5c9161c382a273664265b732b601ea2ccf51
ardaalkan <ardaaa.alk@gmail.com>
2023-10-12 15:52:56 +0300

    create sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 import md5 from "md5";
+import jwt from "jsonwebtoken";
 
 /*
 md5 şifreleme paketi ile birlikte kullanıcının gireceği şifre artık belirli bir
@@ -17,5 +19,37 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
     // res.status(500).json(error.message);
+  }
+};
+
+/* 
+json web token oluşturulur ve auth işlemlerinde kullanılır. 
+verileri güvenli bir şekilde taşımak için kullanılan 
+bir veri yapısıdır ve genellikle kimlik doğrulama 
+ve yetkilendirme amacıyla kullanılır. JWT, JSON formatında veri depolar ve bu 
+veriyi dijital olarak imzalar veya şifreler. 
+*/
+
+/*
+ödevde gönderilen pdf dosyasında bonus ifadesindeki isteklere göre burada
+halihazırda md5 paketi kullanılarak oluşturulmuş şifre database de şifrelenmiş 
+haldedir ve şifre karşılaştırmak amacı ile tekrar burada kullanılır.
+*/
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = md5(password) === validUser.password; // MD5 ile karşılaştır
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
