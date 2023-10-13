e3fa9e86b06b6266bfa0d29c3a3c02e6253db7cb
ardaalkan <ardaaa.alk@gmail.com>
2023-10-13 17:55:11 +0300

    addede sidebar styles/css

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
-import Avatar from "./Avatar/Avatar";
 import { NavLink } from "react-router-dom";
+import { BookPlus, LayoutDashboard, FileSearch } from "lucide-react";
+import Avatar from "./Avatar/Avatar";
 import styles from "./Sidebar.module.css";
-
+/*
+styles ifadesi ile css stil tanımlarının ilişkilendirilmesini kolaylaştırır ve stil sınıflarının
+izole olarak kullanılmasını sağlar yani stil isim çakışmalarını ve kaygıları ortadan kaldırır. 
+*/
 export default function Sidebar() {
   const [width, setWidth] = useState(window.innerWidth);
-  const mobileBreakpoint = 720;
+  const breakpoint = 720;
 
   useEffect(() => {
     window.addEventListener("resize", () => setWidth(window.innerWidth));
@@ -13,23 +17,49 @@ export default function Sidebar() {
       window.removeEventListener("resize", () => setWidth(window.innerWidth));
   }, []);
 
+  // Bu işlev, etkin linkler için "active_link" sınıfını ekler
+  const classNameFunc = ({ isActive }) => (isActive ? styles.activeLink : "");
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarContent}>
         <div className={styles.user}>
           <Avatar></Avatar>
-          <p>hey</p>
+          <p>Welcome</p>
         </div>
         <nav className={styles.links}>
           <ul>
             <li>
-              <NavLink to="/" className={styles.link}>
-                Dashboard
+              <NavLink
+                to="/"
+                activeClassName={styles.activeLink}
+                style={{ textDecoration: "none" }}
+                className={classNameFunc}
+              >
+                <LayoutDashboard style={{ marginRight: "25px" }} />
+                {width > breakpoint && <span>Dashboard</span>}
               </NavLink>
             </li>
             <li>
-              <NavLink to="/create" className={styles.link}>
-                Create
+              <NavLink
+                to="/all"
+                activeClassName={styles.activeLink}
+                style={{ textDecoration: "none" }}
+                className={classNameFunc}
+              >
+                <FileSearch style={{ marginRight: "25px" }} />
+                {width > breakpoint && <span>All Survey</span>}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/add"
+                activeClassName={styles.activeLink}
+                style={{ textDecoration: "none" }}
+                className={classNameFunc}
+              >
+                <BookPlus style={{ marginRight: "25px" }} />
+                {width > breakpoint && <span>Add New Survey</span>}
               </NavLink>
             </li>
           </ul>
