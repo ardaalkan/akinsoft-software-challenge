67e49acbac74c8296cbf590b72e7c2728688359c
ardaalkan <ardaaa.alk@gmail.com>
2023-11-15 01:18:39 +0300

    refactored answer button to copy btn

--- client/src/pages/All/ListingCard/ListingCard.jsx
+++ client/src/pages/All/ListingCard/ListingCard.jsx
@@ -1,11 +1,23 @@
 import styles from "./ListingCard.module.css";
-import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // "ListingCard" adında bir fonksiyonel bileşen tanımlanır
 function ListingCard({ data, onDeleteClick }) {
+  const handleAnswerClick = () => {
+    // Tam URL'yi panoya kopyala
+    const fullURLToCopy = `${window.location.origin}/${data._id}`;
+    navigator.clipboard
+      .writeText(fullURLToCopy)
+      .then(() => {
+        toast.success("URL copied to clipboard!");
+      })
+      .catch((error) => {
+        console.error("Error copying to clipboard:", error);
+      });
+  };
 
   return (
-    // Bileşenin dış sınıfı "styles.listingCard" ile stilendirilir
     <div className={styles.listingCard}>
       <div>
         <h2>{data.name}</h2>
@@ -14,16 +26,16 @@ function ListingCard({ data, onDeleteClick }) {
       </div>
       <div className={styles.listingBtns}>
         {/* Anketi güncelleme sayfasına yönlendiren bir Link oluşturulur */}
-        <Link to={`/update/${data._id}`}>
+        <a href={`/update/${data._id}`}>
           <p>Edit</p>
-        </Link>
+        </a>
         {/* Anketi yanıtlama sayfasına yönlendiren bir Link oluşturulur */}
-        <Link to={`/${data._id}`}>
-          <p>Answer</p>
-        </Link>
-        <Link to={`/${data._id}/response`}>
+        <a href="#" onClick={handleAnswerClick}>
+          <p>Copy Link</p>
+        </a>
+        <a href={`/${data._id}/response`}>
           <p>Replies</p>
-        </Link>
+        </a>
         <span onClick={() => onDeleteClick(data._id)}>Delete</span>
       </div>
     </div>
